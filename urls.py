44d671df724d5ce78add76bba30865468d1ff6cb
Jeremy Lainé <jeremy.laine@m4x.org>
2013-09-03 10:51:31 +0200

    cleanup urls

--- urls.py
+++ urls.py
@@ -1,15 +1,10 @@
-from django.conf.urls import *
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
