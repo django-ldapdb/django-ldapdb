92ee02d49ff9c99aef1c0b3a81c474fdb6f9567f
Jeremy Lainé <jeremy.laine@m4x.org>
2014-07-11 13:11:11 +0200

    also ignore positional arguments

--- ldapdb/backends/ldap/base.py
+++ ldapdb/backends/ldap/base.py
@@ -39,14 +39,14 @@ from django.db.backends.creation import BaseDatabaseCreation
 
 
 class DatabaseCreation(BaseDatabaseCreation):
-    def create_test_db(self, **kwargs):
+    def create_test_db(self, *args, **kwargs):
         """
         Creates a test database, prompting the user for confirmation if the
         database already exists. Returns the name of the test database created.
         """
         pass
 
-    def destroy_test_db(self, **kwargs):
+    def destroy_test_db(self, *args, **kwargs):
         """
         Destroy a test database, prompting the user for confirmation if the
         database already exists. Returns the name of the test database created.
