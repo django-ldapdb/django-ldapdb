8c6e81dbd7c647309c9da32c6dc581a318d2f510
Jeremy Lainé <jeremy.laine@m4x.org>
2014-07-11 13:08:45 +0200

    ignore create_test_db / destroy_test_db kwargs

--- ldapdb/backends/ldap/base.py
+++ ldapdb/backends/ldap/base.py
@@ -39,14 +39,14 @@ from django.db.backends.creation import BaseDatabaseCreation
 
 
 class DatabaseCreation(BaseDatabaseCreation):
-    def create_test_db(self, verbosity=1, autoclobber=False):
+    def create_test_db(self, **kwargs):
         """
         Creates a test database, prompting the user for confirmation if the
         database already exists. Returns the name of the test database created.
         """
         pass
 
-    def destroy_test_db(self, old_database_name, verbosity=1):
+    def destroy_test_db(self, **kwargs):
         """
         Destroy a test database, prompting the user for confirmation if the
         database already exists. Returns the name of the test database created.
