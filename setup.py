63503b22ec1409edeaaceb54b54d7e896ee12cea
Dominik George <nik@naturalnet.de>
2018-07-29 20:16:08 +0200

    Mark as compatible with Python 3.7 in PyPI, advances #165.

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.0",
         "Intended Audience :: Developers",
