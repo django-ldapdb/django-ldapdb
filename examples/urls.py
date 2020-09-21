b23252e7b533f639108f9fb562216d0384d39fdc
dzen <benoit.calvez@polyconseil.fr>
2020-09-21 16:06:54 +0200

    examples: upgrade urls to path() function

--- examples/urls.py
+++ examples/urls.py
@@ -2,13 +2,12 @@
 # This software is distributed under the two-clause BSD license.
 # Copyright (c) The django-ldapdb project
 
-
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, path
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', admin.site.urls),
+    path(r'admin/doc/', include('django.contrib.admindocs.urls')),
+    path(r'admin/', admin.site.urls),
 ]
