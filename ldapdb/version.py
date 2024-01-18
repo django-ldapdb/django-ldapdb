ae5a00277342792918553dc7f898710a3a423386
Alexandre Muller <alexandre.muller@polyconseil.fr>
2024-01-18 13:40:34 +0100

    setup: drop support for Python 3.6 & 3.7

--- ldapdb/version.py
+++ ldapdb/version.py
@@ -2,16 +2,8 @@
 # This software is distributed under the two-clause BSD license.
 # Copyright (c) The django-ldapdb project
 
-import sys
+import importlib.metadata
 
-if sys.version_info < (3, 8):
-    import pkg_resources
-
-    __version__ = pkg_resources.get_distribution('django-ldapdb').version
-
-else:
-    import importlib.metadata
-
-    __version__ = importlib.metadata.version('django-ldapdb')
+__version__ = importlib.metadata.version('django-ldapdb')
 
 VERSION = __version__
